e065027735c62d0b8280edab45aff4eb76b5fcef
DanilaTruskouski <110732655+DanilaTruskouski@users.noreply.github.com>
2022-09-01 18:25:43 +0300

    Add files via upload

--- src/Home_Work_4/StringComparator.java
+++ src/Home_Work_4/StringComparator.java
@@ -1,2 +1,12 @@
-package Home_Work_4;public class StringComparator {
+package Home_Work_4;
+
+import java.util.Comparator;
+
+public class StringComparator implements Comparator<String> {
+
+
+    @Override
+    public int compare(String o1, String o2) {
+        return o1.length()-o2.length();
+    }
 }
