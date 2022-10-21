127ab2749055bded0f8ead65477166da7717f887
zuuuuuuz <110911987+zuuuuuuz@users.noreply.github.com>
2022-10-21 17:45:28 +0300

    update

--- src/Home_Work_7/comparators/ComparatorMap.java
+++ src/Home_Work_7/comparators/ComparatorMap.java
@@ -4,7 +4,7 @@ import java.util.Comparator;
 import java.util.Map;
 
 public class ComparatorMap implements Comparator<Map.Entry<String,Integer>> {
-
+     
     @Override
     public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
         return o2.getValue() - o1.getValue();
